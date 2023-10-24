89d3bed8b171b1e88d121a0345cfc0d43f93e4ce
While-Luke <lukeellis3141@gmail.com>
2023-10-24 17:30:53 +0800

    Big changes

--- hierarchyGPT.py
+++ hierarchyGPT.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-import re
+
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
@@ -11,10 +11,12 @@ load_dotenv()
 def get_code_from_string(s):
     if '```python\n' in s:
         s = s.split('```python\n')[1].split('```')[0]
+    elif 'def' in s:
+        s = s[s.find('def'):]
     return s
 
 # Find languages
-def get_languages(llm, project_idea, verbose = False):
+def get_languages(llm, info, verbose = False):
     if input("Do you know what languages you want to use [Y/N]?").upper() == "Y":
         languages = input("Which languages would you like to use? ")
     else:
@@ -24,41 +26,67 @@ def get_languages(llm, project_idea, verbose = False):
             llm=llm,
             prompt=prompt_template
         )
-        languages = llm_chain(project_idea)["text"]
+        if verbose > 1:
+            print("Languages prompt: ")
+            print(prompt.format(**info))
+
+        languages = llm_chain(info)["text"]
         if verbose:
             print(languages)
+
     languages = languages.replace(" ", "").split(",")
     return languages
 
 
 # Find files and directories
-def get_files(llm, project_idea, languages, verbose = False):
+def get_files(llm, info, verbose = False):
     if input("Will this project be contained in one single file? [Y/N]").upper() == "Y":
         file = input("What will the file be called? ")
-        return [file]
+        return [file], True
     prompt = prompts.files_prompt
     prompt_template = PromptTemplate.from_template(prompt)
     llm_chain = LLMChain(
         llm=llm,
         prompt=prompt_template
     )
-    files = llm_chain({'description': project_idea, 'languages': languages, 'file': file})["text"]
-    files = files.replace(" ", "").split(",")
+    if verbose > 1:
+        print("Files prompt: ")
+        print(prompt.format(**info))
+
+    response = llm_chain(info)["text"]
+
+    files = {}
+    for resp in response.split("\n"):
+        if resp == '': #Somethimes uses double newlines
+            continue
+        resp = resp.split(":")
+        file_name = resp[0].replace(" ", "")
+        file_description = resp[1]
+        files[file_name] = file_description
     if verbose:
         print(files)
-    return files
+    return files, False
 
 # Find methods inside files
-def get_methods(llm, project_idea, languages, file, verbose = False):
-    prompt = prompts.methods_prompt
+def get_methods(llm, info, singlefile, verbose = False):
+    if singlefile:
+        prompt = prompts.methods_prompt
+    else:
+        prompt = prompts.methods_prompt_extra
     prompt_template = PromptTemplate.from_template(prompt)
     llm_chain = LLMChain(
         llm=llm,
         prompt=prompt_template
     )
-    response = llm_chain({'description': project_idea, 'languages': languages, 'file': file})["text"]
+    if verbose > 1:
+        print("Methods prompt: ")
+        print(prompt.format(**info))
+
+    response = llm_chain(info)["text"]
     methods = {}
     for resp in response.split("\n"):
+        if resp == '': #Somethimes uses double newlines
+            continue
         resp = resp.split(":")
         method_name = resp[0].replace(" ", "")
         method_description = resp[1]
@@ -68,35 +96,51 @@ def get_methods(llm, project_idea, languages, file, verbose = False):
     return methods
 
 # Find imports and globals within file
-def get_globals(llm, project_idea, languages, file, methods, verbose = False):
-    prompt = prompts.globals_prompt
+def get_globals(llm, info, singlefile, verbose = False):
+    if singlefile:
+        prompt = prompts.globals_prompt
+    else:
+        prompt = prompts.globals_prompt_extra
     prompt_template = PromptTemplate.from_template(prompt)
     llm_chain = LLMChain(
         llm=llm,
         prompt=prompt_template
     )
-    globals = llm_chain({'description': project_idea, 'languages': languages, 'file': file, 'methods': methods})["text"]
+    if verbose > 1:
+        print("Globals prompt: ")
+        print(prompt.format(**info))
+
+    globals = llm_chain(info)["text"]
     if verbose:
         print(globals)
     globals = get_code_from_string(globals)
-    with open(file, "a") as f:
+    with open(info['file'], "a") as f:
         f.write(globals)
+        f.write("\n")
     return globals
 
 # Implement methods
-def fill_method(llm, project_idea, languages, file, methods, globals, method, explanation, verbose = False):
-    prompt = prompts.coding_prompt
+def fill_method(llm, info, singlefile, verbose = False):
+    if singlefile:
+        prompt = prompts.coding_prompt
+    else:
+        prompt = prompts.coding_prompt_extra
     prompt_template = PromptTemplate.from_template(prompt)
     llm_chain = LLMChain(
         llm=llm,
         prompt=prompt_template
     )
-    code = llm_chain({'description': project_idea, 'languages': languages, 'file': file, 'methods': methods, 'globals': globals, 'method': method, 'explanation': explanation})["text"]
+    if verbose > 1:
+        print("Coding prompt: ")
+        print(prompt.format(**info))
+
+    code = llm_chain(info)["text"]
     if verbose:
         print(code)
     
     code = get_code_from_string(code)
-    with open(file, "a") as f:
+
+    with open(info['file'], "a") as f:
         f.write(code)
         f.write("\n")
 
@@ -105,29 +149,46 @@ def fill_method(llm, project_idea, languages, file, methods, globals, method, ex
 
 def main(verbose = False):
     llm = ChatOpenAI()
+    info = {}
 
-    #project_idea = input("What project would you like to make? ")
-    project_idea = "A simple tik-tak-toe game that a user can play with a command line interface."
+    project_idea = input("What project would you like to make? ")
+    #project_idea = "A simple tik-tak-toe game that a user can play with a command line interface."
+    info["description"] = project_idea
 
-    #languages = get_languages(llm, project_idea, verbose)
-    languages = ["Python"]
+    languages = get_languages(llm, info, verbose)
+    info["languages"] = languages
 
-    #files = get_files(llm, project_idea, verbose)
-    files = ["tiktaktoe.py"]
+    files, singlefile = get_files(llm, info, verbose)
 
-    methods = get_methods(llm, project_idea, languages, files[0], verbose)
-    #methods = ['start_game', 'print_board', 'make_move', 'check_win', 'switch_player', 'is_board_full', 'is_valid_move']
+    if singlefile:
+        info["file"] = files[0]
 
-    globals = get_globals(llm, project_idea, languages, files[0], methods, verbose)
-    '''globals = """import random
+        methods = get_methods(llm, info, singlefile, verbose)
+        info["methods"] = methods
 
-# Global Variables
-board = [' ' for _ in range(9)]
-players = ['X', 'O']
-current_player = random.choice(players)"""'''
+        globals = get_globals(llm, info, singlefile, verbose)
+        info["globals"] = globals
 
-    for method, explanation in methods.items():
-        fill_method(llm, project_idea, languages, files[0], methods, globals, method, explanation, verbose)
+        for method, explanation in methods.items():
+            info["method"] = method
+            info["explanation"] = explanation
+            fill_method(llm, info, singlefile, verbose)
+
+    else:
+        for file, filedesc in files.items():
+            info["file"] = file
+            info["filedesc"] = filedesc
+
+            methods = get_methods(llm, info, singlefile, verbose)
+            info["methods"] = methods
+
+            globals = get_globals(llm, info, singlefile, verbose)
+            info["globals"] = globals
+
+            for method, explanation in methods.items():
+                info["method"] = method
+                info["explanation"] = explanation
+                fill_method(llm, info, singlefile, verbose)
 
 
 
--- prompts.py
+++ prompts.py
@@ -2,7 +2,7 @@ languages_prompt = """I have an idea for a project that I would like to make. He
 
 {description}
 
-What languages and software do you think would be best to use to create this project? List only the results separated by commas, do not add any other information or explanation.
+What programming languages and software do you think would be best to use to implement this project? If there is more than one language that is necessary, list only the results separated by commas, do not add any other information or explanation.
 """
 
 
@@ -15,7 +15,13 @@ It is going to use the following programming languages and software:
 
 {languages}
 
-What files do you think I would need to make to implement my ideas and methods into? List only the filenames separated by commas, do not add any other information or explanation.
+What files do you think I would need to make to implement my ideas and methods into? Provide a short explanation as to what each file should achieve. List the results in the following form:
+
+file_name: Explanation
+file_name: Explanation
+and so on...
+
+Do not add any other information or explanation.
 """
 
 
@@ -28,13 +34,34 @@ It is going to use the following languages and software:
 
 {languages}
 
-Inside of the file "{file}", what methods would need to be implemented to make the project work? List the results in the following form:
+What methods would need to be implemented to make the project work? Provide a short explanation as to what each method should achieve. List the results in the following form:
 
 method_name: Explanation
 method_name: Explanation
 and so on...
 
-Do not add any other information or explanation.
+Don't forget to add a 'main' method to run the whole program. Do not add any other information or explanation.
+"""
+
+methods_prompt_extra = """I have an idea for a project that I would like to make. Here is a description of the project:
+
+{description}
+
+It is going to use the following languages and software:
+
+{languages}
+
+I need to implement the file "{file}" which should achieve the following:
+
+{filedesc}
+
+What methods would need to be implemented inside of this file to make the project work? Provide a short explanation as to what each method should achieve. List the results in the following form:
+
+method_name: Explanation
+method_name: Explanation
+and so on...
+
+Don't forget to add a 'main' method to run the whole program. Do not add any other information or explanation.
 """
 
 
@@ -47,16 +74,58 @@ It is going to use the following languages and software:
 
 {languages}
 
-Inside of the file "{file}", it is going to contain the following methods:
+The program will contain the following methods:
 
 {methods}
 
-Implement what the very top of this file should look like, including the import statements and any global variables if necessary. Do not implement any methods or any other code, and do not add any other information or explanation.
+Implement what the very top of this file should look like, including the import statements, constants and any global variables if necessary. Do not implement any methods or any other code, and do not add any other information or explanation.
+"""
+
+globals_prompt_extra = """I have an idea for a project that I would like to make. Here is a description of the project:
+
+{description}
+
+It is going to use the following languages and software:
+
+{languages}
+
+I need to implement the file "{file}" which should achieve the following:
+
+{filedesc}
+
+Inside of the {file}, it is going to contain the following methods:
+
+{methods}
+
+Implement what the very top of this file should look like, including the import statements, constants and any global variables if necessary. Do not implement any methods or any other code, and do not add any other information or explanation.
 """
 
 
 
 coding_prompt = """I have an idea for a project that I would like to make. Here is a description of the project:
+
+{description}
+
+It is going to use the following languages and software:
+
+{languages}
+
+The program will contain the following methods:
+
+{methods}
+
+This file will also contain the following imports and globals:
+
+{globals}
+
+Implement the method {method}. Here is a description of what this method should do:
+
+{explanation}
+
+Do not give any extra information and explanation about this method, just the code for this one method.
+"""
+
+coding_prompt_extra = """I have an idea for a project that I would like to make. Here is a description of the project:
 
 {description}
 
